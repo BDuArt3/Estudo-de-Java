215c5f1226ccdcd80c7cadf883728b6d4a5b501c
BDuArt3 <brunoduartesilveira909@gmail.com>
2026-07-19 01:39:27 -0300

    Adicionar opção para listar alunos e melhorar a exibição de alunos cadastrados

--- TrilhaIniciante/ProjetoIniciante/Main.java
+++ TrilhaIniciante/ProjetoIniciante/Main.java
@@ -17,6 +17,8 @@ public class Main {
             System.out.println("2 - Calcular media de um aluno.");
             System.out.println("3 - Calcular media de todos os alunos.");
             System.out.println("4- Exibir aluno com maior media");
+            System.out.println("5 - Listar alunos.");
+            System.out.println("0 - Sair");
             opcao = sc.nextInt();
             sc.nextLine();
             if (opcao == 1){
@@ -39,14 +41,21 @@ public class Main {
             if (opcao == 4){
                 ExibirAlunoComMaiorMedia(alunos);
             }
+            if (opcao == 5){
+                ListarALunos(alunos);
+            }
             
         } while (opcao != 0);
 
+        return;
+
     }
-
-    public static void AlunoMaiorMedia (Aluno[] alunos){
+    public static void ListarALunos(Aluno[] alunos) {
+        System.out.println("Listando alunos:");
         for (Aluno aluno : alunos){
-
+            if (aluno != null){
+                System.out.println(aluno.getNome());
+            }
         }
     }
 
